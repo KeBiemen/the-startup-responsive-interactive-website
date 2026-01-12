23a725821d50a468aac2507fdb55b4e01ab0a3d0
KeBiemen <ke.van.biemen@hva.nl>
2026-01-12 21:43:23 +0100

    interactie toevoeging (#13)

--- werktijden.js
+++ werktijden.js
@@ -1,10 +1,12 @@
+
+//  selecteer de articles als element, 
 let uitklapknop = document.querySelectorAll('article');
 
 /* hij loopt door alle buttons heen */
 uitklapknop.forEach(function(buttons){
-    /* als iemand op een van de like buttons clickt gebeurt er een function */
+    /* als je klikt op een article gebeurt er een function */
 buttons.addEventListener("click", function(){
-        /* function is togglen met voor alle classes met fill*/
+        /* koppel een class toe aan het geselecteerde element*/
     buttons.classList.toggle("uitgeklapt")
     })
 })
