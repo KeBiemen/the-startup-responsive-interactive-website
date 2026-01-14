99323f135311591146c7bbd93e175a5aa299bb1a
KeBiemen <ke.van.biemen@hva.nl>
2026-01-14 18:39:14 +0100

    herhalende code weg (fixed #14)

--- werktijden.js
+++ werktijden.js
@@ -1,12 +1,13 @@
 
 //  selecteer de articles als element, 
-let uitklapknop = document.querySelectorAll('article');
+const uitKlapKnop = document.querySelectorAll('article');
 
 /* hij loopt door alle buttons heen */
-uitklapknop.forEach(function(buttons){
+uitKlapKnop.forEach(function(buttons){
     /* als je klikt op een article gebeurt er een function */
 buttons.addEventListener("click", function(){
         /* koppel een class toe aan het geselecteerde element*/
     buttons.classList.toggle("uitgeklapt")
     })
 })
+
