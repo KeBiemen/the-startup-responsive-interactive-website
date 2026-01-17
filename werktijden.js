964e60ffb602be1547d5a0b0898276947de64744
KeBiemen <ke.van.biemen@hva.nl>
2026-01-17 07:48:30 +0100

    lees minder knop (#13)

--- werktijden.js
+++ werktijden.js
@@ -2,12 +2,19 @@
 //  selecteer de articles als element, 
 const uitKlapKnop = document.querySelectorAll('article');
 
+
 /* hij loopt door alle buttons heen */
 uitKlapKnop.forEach(function(buttons){
+    
     /* als je klikt op een article gebeurt er een function */
 buttons.addEventListener("click", function(){
         /* koppel een class toe aan het geselecteerde element*/
     buttons.classList.toggle("uitgeklapt")
     })
+
+
 })
+
+
+
 
